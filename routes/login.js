51e94a4c658ca749b56ff427698222416d9b6b10
Cynthia <cynthia.philippe@gmail.com>
2026-07-17 14:58:02 +0200

    Fix route signup manquante + signin sans firstname

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
 require('../models/connection')
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
-
 const uid2 = require('uid2');
-
 const bcrypt = require('bcrypt')
+
+router.post('/signup', (req,res) => {
+    if (!checkBody(req.body, ['firstname', 'username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+    }
+    User.findOne({firstname: req.body.firstname, username: req.body.username})
+    .then(data => {
+        if(data === null) {
+            const hash = bcrypt.hashSync(req.body.password, 10);
+            const newUser = new User ({
+                firstname: req.body.firstname,
+                username : req.body.username,
+                password: hash,
+                token: uid2(32),
+            })
+            newUser.save()
+            .then(newDoc => {
+                res.json({result: true, token: newDoc.token, firstname: newDoc.firstname, username: newDoc.username})
+            })
+        } else {
+            res.json({result: false, error: 'User already exists'})
+        }
+    })
+});
 
 router.post('/signin', (req,res) => {
     if (!checkBody(req.body, ['username', 'password'])) {
@@ -22,33 +44,6 @@ router.post('/signin', (req,res) => {
               res.json({ result: false, error: 'User not found or wrong password' });
             }
     })
-}); //fermeture de la router
-
-router.post('/signin', (req,res) => {
-    if (!checkBody(req.body, ['firstname', 'username', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
-    return;
-    }
-
-    User.findOne({firstname: req.body.firstname, username: req.body.username})
-    .then(data => {
-        if (data && bcrypt.compareSync(req.body.password, data.password)) {
-              res.json({ result: true, token: data.token });
-            } else {
-              res.json({ result: false, error: 'User not found or wrong password' });
-            }
-    })
-
-}); //fermeture de la router
+});
 
 module.exports = router;
-
-router.post('/tweets/:token', (req, res) => {
-  User.findOne({ token: req.params.token }).then(data => {
-    if (data) {
-      res.json({ result: true, tweet: data.tweet });
-    } else {
-      res.json({ result: false, error: 'User not found' });
-    }
-  });
-});
